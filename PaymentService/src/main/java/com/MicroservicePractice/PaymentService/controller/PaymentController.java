299a098a8254f3f59f8ebea8a916b26fbf5f633a
Rapheo <abdullah.rapheo@g.bracu.ac.bd>
2025-11-17 16:06:41 +0600

    added resource server for oauth2 in paymentService

--- PaymentService/src/main/java/com/MicroservicePractice/PaymentService/controller/PaymentController.java
+++ PaymentService/src/main/java/com/MicroservicePractice/PaymentService/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.MicroservicePractice.PaymentService.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
         return new ResponseEntity<>(
@@ -22,7 +24,7 @@ public class PaymentController {
                 HttpStatus.OK
         );
     }
-
+    @PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @GetMapping("/order/{orderId}")
     public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
         return new ResponseEntity<>(
