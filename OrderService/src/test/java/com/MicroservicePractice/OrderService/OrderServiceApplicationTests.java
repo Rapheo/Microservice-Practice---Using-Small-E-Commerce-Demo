adc935d7cbc658f49833e798fb00118e210f4406
Rapheo <abdullah.rapheo@g.bracu.ac.bd>
2025-11-19 11:42:17 +0600

    added junit test... added testcase for getOrderDetails

--- OrderService/src/test/java/com/MicroservicePractice/OrderService/OrderServiceApplicationTests.java
+++ OrderService/src/test/java/com/MicroservicePractice/OrderService/OrderServiceApplicationTests.java
@@ -1,13 +1,6 @@
 package com.MicroservicePractice.OrderService;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OrderServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
